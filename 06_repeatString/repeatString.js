1d86233a7656bbf2c7f81fc04bbdae9a6b619204
Noc707 <236208980+Noc707@users.noreply.github.com>
2025-10-27 03:46:49 +0000

    java seven
    Done

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function (word, times) {
+if (items < 0) return "ERROR";
+let string = "";
+for (let i = 0; i < times; i++) {
+string += word;
+}
+return string;
 };
 
+
 // Do not edit below this line
-module.exports = repeatString;
+module.exports = repeatString; {
+
+
+}
